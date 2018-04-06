8add32bb03f7ac31e24e6dcf5a6813594fa5522d
Zico郭 <21520993@qq.com>
2018-04-07 01:57:21 +0800

    v0.0.4
    增加微信授权等内容

---
+++ src/pages/components/fullCalendar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id='fullCalendar'>
+    <vuefullCalendar :events="fcEvents" locale="en">
+    </vuefullCalendar>
+  </div>
+</template>
+
+<script>
+import vuefullCalendar from "vue-fullcalendar";
+export default {
+  name: "fullCalendar",
+  data() {
+    return {
+      fcEvents: fcEvents
+    };
+  },
+  components: {
+    vuefullCalendar
+  }
+};
+const fcEvents = [
+  {
+    title: "event1",
+    start: "2018-04-01",
+    cssClass: "family",
+    YOUR_DATA: {}
+  },
+  {
+    title: "event2",
+    start: "2018-04-12",
+    end: "2018-07-03",
+    cssClass: ["family", "career"],
+    YOUR_DATA: {}
+  }
+];
+</script>
+
+<style scoped>
+  .comp-full-calendar {
+    padding: 20px 0;
+  }
+</style>
