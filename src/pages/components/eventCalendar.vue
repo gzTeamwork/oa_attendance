c16d56fe2480522d3cbd2b20e45275b4aac162f6
Zico郭 <21520993@qq.com>
2018-04-07 16:25:04 +0800

    Revert "Merge branch 'master' of https://github.com/gzTeamwork/oa_attendance"
    This reverts commit 55b516a5758632dc8e3a41f107e0b26e6d2d92c7, reversing
    changes made to 8add32bb03f7ac31e24e6dcf5a6813594fa5522d.

--- src/pages/components/eventCalendar.vue
+++ src/pages/components/eventCalendar.vue
@@ -1,6 +1,5 @@
 <template>
   <div id='vueEventsCalendar'>
-<<<<<<< HEAD
     <vue-event-calendar :events="calenderEvents"
                         @day-changed="handleDayChanged"
                         @month-changed="handleMonthChanged"
@@ -12,13 +11,6 @@
           <!-- 这里拿到的是传入的单个event所有数据 -->
           <div v-for="(user , oindex) in event.onduty"
                :key="oindex">
-=======
-    <vue-event-calendar :events="calenderEvents" @day-changed="handleDayChanged" @month-changed="handleMonthChanged" :style='{"margin-bottom":"2em"}'>
-      <template slot-scope="props">
-        <div v-for="(event, index) in props.showEvents" class="event-item">
-          <!-- 这里拿到的是传入的单个event所有数据 -->
-          <div v-for="(user , oindex) in event.onduty">
->>>>>>> d47e512243f68c3f25188df21dcee9bbe1127d97
             {{user}}
           </div>
         </div>
@@ -28,7 +20,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 // 引入日历插件
 import Vue from "vue";
 import "vue-event-calendar/dist/style.css";
@@ -86,50 +77,4 @@ export default {
 .item .is-event {
   border-radius: 0 !important;
 }
-=======
-  import eventBus from "@/libs/eventBus.js";
-  import dateCalendar from "@/libs/dateCalendar.js";
-  dateCalendar.init();
-
-  export default {
-    name: 'vueEventsCalendar',
-    data() {
-      return {
-        calenderEvents: dateCalendar.events,
-        appToken: null
-      }
-    },
-    created() {
-      let vm = this;
-      let today = dateCalendar.today;
-      vm.$EventCalendar.toDate(today);
-      eventBus.$on('appToken', function (data) {
-        vm.appToken = data;
-      });
-    },
-    beforeMount() {},
-    methods: {
-      //  日期切换事件
-      handleDayChanged: (dateEvent) => {
-        console.log(dateEvent);
-      },
-      //  月份切换事件
-      handleMonthChanged: (dateEvent) => {
-        console.log(dateEvent)
-      },
-      getToday: function () {
-        let vueDate = new Date();
-        return vueDate.getFullYear() + '/' + (vueDate.getMonth() + 1) + '/' + vueDate.getDate();
-      },
-    }
-  }
-
-</script>
-
-<style scoped>
-  .item .is-event {
-    border-radius: 0 !important;
-  }
-
->>>>>>> d47e512243f68c3f25188df21dcee9bbe1127d97
 </style>
--- src/pages/components/fullCalendar.vue
+++ src/pages/components/fullCalendar.vue
@@ -6,7 +6,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 import vuefullCalendar from "vue-fullcalendar";
 export default {
   name: "fullCalendar",
@@ -34,35 +33,6 @@ const fcEvents = [
     YOUR_DATA: {}
   }
 ];
-=======
-  import vuefullCalendar from 'vue-fullcalendar';
-  export default {
-    name: 'fullCalendar',
-    data() {
-      return {
-        fcEvents: fcEvents
-      }
-    },
-    components: {
-      vuefullCalendar
-    }
-  }
-  const fcEvents = [{
-      title: 'event1',
-      start: '2018-04-01',
-      cssClass: 'family',
-      YOUR_DATA: {}
-    },
-    {
-      title: 'event2',
-      start: '2018-04-12',
-      end: '2018-07-03',
-      cssClass: ['family', 'career'],
-      YOUR_DATA: {}
-    }
-  ]
-
->>>>>>> d47e512243f68c3f25188df21dcee9bbe1127d97
 </script>
 
 <style scoped>
